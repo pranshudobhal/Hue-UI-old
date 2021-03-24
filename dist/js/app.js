d1fa463c721e3e37ae74447aa32f479910891574
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-03-24 22:04:31 +0530

    styles: updated image component

--- dist/js/app.js
+++ dist/js/app.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.menu-nav__item');
+const logo = document.querySelector('.logo');
 
 let showMenu = false;
 
@@ -10,12 +11,14 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
+    logo.style.display = 'none';
     hamburger.classList.add('open');
     nav.classList.add('open');
     menuNav.classList.add('open');
     navItems.forEach((item) => item.classList.add('open'));
     showMenu = true;
   } else {
+    logo.style.display = 'block';
     hamburger.classList.remove('open');
     nav.classList.remove('open');
     menuNav.classList.remove('open');
