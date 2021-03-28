ea67977a807adf29fa27cb0c1c73d5d6084ea6e8
Pranshu Dobhal <pranshu.dobhal@gmail.com>
2021-03-29 04:15:13 +0530

    fixed navbar and refactored code

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburger = document.querySelector('.menu-btn__burger');
-const nav = document.querySelector('.nav');
-const menuNav = document.querySelector('.menu-nav');
-const navItems = document.querySelectorAll('.menu-nav__item');
-const logo = document.querySelector('.logo');
+const sidenav = document.getElementById('mySidenav');
 
 let showMenu = false;
 
@@ -11,18 +8,12 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
   if (!showMenu) {
-    logo.style.display = 'none';
     hamburger.classList.add('open');
-    nav.classList.add('open');
-    menuNav.classList.add('open');
-    navItems.forEach((item) => item.classList.add('open'));
+    sidenav.style.width = '250px';
     showMenu = true;
   } else {
-    logo.style.display = 'block';
     hamburger.classList.remove('open');
-    nav.classList.remove('open');
-    menuNav.classList.remove('open');
-    navItems.forEach((item) => item.classList.remove('open'));
+    sidenav.style.width = '0';
     showMenu = false;
   }
 }
